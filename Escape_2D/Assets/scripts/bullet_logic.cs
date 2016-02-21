f4fbded51442f5eca6c9cca5f6e9ea134a9ffcb7
ctdh24 <ctdh24@gmail.com>
2016-02-20 17:03:03 -0800

    created a basic room

---
+++ Escape_2D/Assets/scripts/bullet_logic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class bullet_logic : MonoBehaviour {
+
+    private Animator b_anim;
+    // Use this for initialization
+    void Start () {
+        b_anim = gameObject.GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        b_anim.Play("FireAnimation");
+	}
+}
---
+++ Escape_2D/Assets/scripts/teleporter_script.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class teleporter_script : MonoBehaviour {
+    public GameObject pointA;
+    public GameObject pointB;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
