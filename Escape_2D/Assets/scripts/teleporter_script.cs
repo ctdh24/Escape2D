bbd2de59d8f726f2c425225d1b258824430d9910
ctdh24 <ctdh24@gmail.com>
2016-02-24 19:33:45 -0800

    added teleporting, fixed character hitbox

--- Escape_2D/Assets/scripts/teleporter_script.cs
+++ Escape_2D/Assets/scripts/teleporter_script.cs
@@ -4,13 +4,9 @@ using System.Collections;
 
 public class teleporter_script : MonoBehaviour {
     public GameObject destination;
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        col.gameObject.transform.position = destination.transform.position;
+    }
 }
