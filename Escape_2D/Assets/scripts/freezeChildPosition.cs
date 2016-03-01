0d965f000c1c0f72a88ec7f7acd23361569c76e9
ctdh24 <ctdh24@gmail.com>
2016-02-29 18:32:47 -0800

    fixed localposition problem with ground check and wall check

---
+++ Escape_2D/Assets/scripts/freezeChildPosition.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class freezeChildPosition : MonoBehaviour {
+    private Vector3 initialPosition;
+	// Use this for initialization
+	void Start () {
+        initialPosition = gameObject.transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (gameObject.transform.localPosition != initialPosition)
+        {
+            gameObject.transform.localPosition = initialPosition;
+        }
+	}
+}
