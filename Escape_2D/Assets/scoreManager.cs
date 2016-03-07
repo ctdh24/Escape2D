ad72dc382a9f50f24d8448e969614e2de6dce38d
ctdh24 <ctdh24@gmail.com>
2016-03-06 19:55:49 -0800

    added score handler

---
+++ Escape_2D/Assets/scoreManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class scoreManager : MonoBehaviour {
+    public static int score;
+    Text text;
+	// Use this for initialization
+	void Awake () {
+        text = GetComponent<Text>();
+        score = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        text.text = "Score: " + score;
+	}
+}
