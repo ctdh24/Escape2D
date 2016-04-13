8aaa8c74060ac0a2f42c6cc649c411e0076e86fb
ctdh24 <ctdh24@gmail.com>
2016-04-13 16:54:16 -0700

    fixed infinite jumping bug

--- Escape_2D/Assets/scripts/player/groundCheck.cs
+++ Escape_2D/Assets/scripts/player/groundCheck.cs
@@ -9,17 +9,30 @@ public class groundCheck : MonoBehaviour {
     {
         player = gameObject.GetComponentInParent<playerController2D>();
     }
+
+    void Update()
+    {
+        GameObject[] bounds = GameObject.FindGameObjectsWithTag("BoundaryManager");
+        foreach(GameObject b in bounds)
+        {
+            Physics2D.IgnoreCollision(gameObject.GetComponent<Collider2D>(), b.GetComponent<Collider2D>());
+            foreach(Transform child in b.transform)
+            {
+                Physics2D.IgnoreCollision(gameObject.GetComponent<Collider2D>(), child.GetComponent<Collider2D>());
+            }      
+        }
+    }
+
     void OnTriggerEnter2D(Collider2D col)
     {
-
         player.p_grounded = true;
         Debug.Log("Landed");
     }
-    /*void OnTriggerStay2D(Collider2D col)
+    void OnTriggerStay2D(Collider2D col)
     {
-        if(col.name != "Player")
-            player.p_grounded = true;
-    }*/
+        player.p_grounded = true;
+        Debug.Log("Stay: " + col.name);
+    }
     void OnTriggerExit2D(Collider2D col)
     {
         player.p_grounded = false;
